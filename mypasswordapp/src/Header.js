6fe67d7154740aed2652e46e2d014335cd73cffb
Brian Kan <BrianKan00@hotmail.com>
2020-01-30 18:25:34 -0500

    Added React

---
+++ mypasswordapp/src/Header.js
@@ -0,0 +1,14 @@
+// Header.js
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Welcome to the Toronto Parks' website</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
